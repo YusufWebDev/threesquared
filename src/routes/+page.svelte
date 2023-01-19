54142e277246032aebf64a5e1f94aa2e1dfdf3ae
YusufWebDev <65045006+YusufWebDev@users.noreply.github.com>
2023-01-19 15:35:10 +0200

    moved images
    added image to the static folder

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,18 @@
+<script>
+	let image = "/images/coming.png";
+</script>
+
 <div class="container">
 	<h1>Welcome to Three Squared</h1>
-	<img src="./src/images/coming.webp" alt="" srcset="" />
+	<img src={image} alt="coming soon" srcset="" />
 </div>
 
 <style>
 	.container {
 		display: grid;
 		place-items: center;
+	}
+	.img {
+		mix-blend-mode: screen;
 	}
 </style>
