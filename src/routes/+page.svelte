ea8000d5a67441bfa57f846e73f5a29139c0bc04
YusufWebDev <65045006+YusufWebDev@users.noreply.github.com>
2023-01-19 13:22:56 +0200

    initial  commit
    only added a coming soon image and a welcome text

---
+++ src/routes/+page.svelte
@@ -0,0 +1,15 @@
+<script>
+	let comingSoon = "/src/images/coming.webp";
+</script>
+
+<div class="container">
+	<h1>Welcome to Three Squared</h1>
+	<img src={comingSoon} alt="" srcset="" />
+</div>
+
+<style>
+	.container {
+		display: grid;
+		place-items: center;
+	}
+</style>
